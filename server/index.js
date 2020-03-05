30226d59cfb78d56acf54ff5de3018a7763f4a1a
Mihai Predoiu <predoiumihai@gmail.com>
2020-03-05 02:12:45 +0200

    Add more dummy endpoints

--- server/index.js
+++ server/index.js
@@ -5,8 +5,20 @@ const express = require('express')
 const { PORT = '8080' } = process.env
 const app = express()
 
-app.use((req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.send('GET request to homepage')
 })
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.post('/', (req, res) => {
+  res.send('POST request to homepage')
+})
+
+app.get('/api/users', (req, res) => {
+  res.send('request to GET all users')
+})
+
+app.post('/api/users', (req, res) => {
+  res.send('request to add one user users')
+})
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`))
